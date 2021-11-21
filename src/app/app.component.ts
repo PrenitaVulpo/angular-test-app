2583327fd41f2ff91d1d35b2086cab65b3dff659
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-11-21 01:41:08 -0300

    add template and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [
+    `
+      h1 {
+        color: darkblue;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   title = 'angular-test-app';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { Servercomponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
-  declarations: [AppComponent, Servercomponent],
+  declarations: [AppComponent, Servercomponent, ServersComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
